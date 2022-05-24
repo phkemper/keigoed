fa27bf32bda8f52a83644a014ccbb2a32a4ebf13
Paul Kemper <phkemper@gmail.com>
2022-05-24 22:10:20 +0200

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 <x-guest-layout>
 
 	<form action="/" method="post">
+		@csrf
 		<div class="form-group">
 			<label for="pin">{{ __('player.enterpin') }}</label>
 			<input type="text" class="form-control" id="pin" maxlength="6" placeholder="------" autofocus>
