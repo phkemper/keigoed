7712a0d8a4997ef2d7cafe3aada2e042e4bbc86f
Paul Kemper <phkemper@gmail.com>
2022-05-25 17:25:27 +0200

    d

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,21 +16,19 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+    <body>
+    	<div id="container">
+            <div id="logo">
+            	<img src="{{ asset('img/keigoed-quiz-tut-logo.svg') }}"/>
+            </div>
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+            
+            @include('layouts.navigation')
+            
         </div>
 
         <!-- Scripts -->
