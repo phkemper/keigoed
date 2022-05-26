1372dc7a9e88130c68886d3e2fc66d7e81b8452c
Paul Kemper <phkemper@gmail.com>
2022-05-26 12:08:38 +0200

    d

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,7 +2,7 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
+        <div class="font-medium text-danger">
             {{ __('auth.wrong') }}
         </div>
 
