cb0c9c0617adff278c6f5350ac3112a270aeb919
Paul Kemper <phkemper@gmail.com>
2022-05-26 12:17:25 +0200

    d

--- resources/views/quizzes/index.blade.php
+++ resources/views/quizzes/index.blade.php
@@ -6,21 +6,19 @@
    	    <p class="status">{{ session('status') }}</p>
     @endif
 	@if ( $quizzes )
-    	<table id="quizzes">
-    		<tbody>
-    			@foreach ($quizzes as $quiz)
-    				<tr>
-    					<td class="quizactions">
-    						<a class="btn btn-primary edit" href="/quizzes/{{ $quiz->id }}" title="{{ __('quiz.actionedit') }}"><i class="bi-pencil-fill"></i></a>
-    						<a class="btn btn-info edit" href="/quizzes/{{ $quiz->id }}/questions" title="{{ __('quiz.actionquestions') }}"><i class="bi-question-circle"></i></a>
-    						<a class="btn btn-info edit" href="/quizzes/{{ $quiz->id }}/run" title="{{ __('quiz.actionrun') }}"><i class="bi-play"></i></a>
-    						<a class="btn btn-danger delete" href="/quizzes/{{ $quiz->id }}/delete" title="{{ __('quiz.actiondelete') }}"><i class="bi-trash-fill"></i></a>
-    					</td>
-    					<td class="quizname">{{ $quiz->name }}</td>
-    				</tr>
-    			@endforeach
-    		</tbody>
-    	</table>
+    	<div id="quizzes">
+			@foreach ($quizzes as $quiz)
+				<div class="quiz">
+					<div class="quizname">{{ $quiz->name }}</div>
+					<div class="actions">
+						<a class="btn btn-primary edit" href="/quizzes/{{ $quiz->id }}" title="{{ __('quiz.actionedit') }}"><i class="bi-pencil-fill"></i></a>
+						<a class="btn btn-info edit" href="/quizzes/{{ $quiz->id }}/questions" title="{{ __('quiz.actionquestions') }}"><i class="bi-question-circle"></i></a>
+						<a class="btn btn-info edit" href="/quizzes/{{ $quiz->id }}/run" title="{{ __('quiz.actionrun') }}"><i class="bi-play"></i></a>
+						<a class="btn btn-danger delete" href="/quizzes/{{ $quiz->id }}/delete" title="{{ __('quiz.actiondelete') }}"><i class="bi-trash-fill"></i></a>
+					</div>
+				</div>
+			@endforeach
+    	</div>
     @endif
 	<a id="add" class="btn btn-primary" href="/quizzes/create">{{ __('quiz.add') }}</a>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,12 @@ Route::get('/quizzes/{id}/delete', [App\Http\Controllers\QuizController::class, 
 Route::post('/quizzes/{id}/delete', [App\Http\Controllers\QuizController::class, 'destroy'])->middleware(['auth'])->name('quizzes.destroy');
 Route::get('/quizzes/{id}/run', [App\Http\Controllers\QuizController::class, 'run'])->middleware(['auth'])->name('quizzes.run');
 
+Route::get('/quizzes/{id}/questions', [App\Http\Controllers\QuestionController::class, 'index'])->middleware(['auth'])->name('questions.index');
+Route::get('/quizzes/{id}/questions/create', [App\Http\Controllers\QuestionController::class, 'create'])->middleware(['auth'])->name('questions.create');
+Route::post('/quizzes/{id}/questions', [App\Http\Controllers\QuestionController::class, 'store'])->middleware(['auth'])->name('questions.store');
+Route::get('/quizzes/{id}/questions/{qid}', [App\Http\Controllers\QuestionController::class, 'show'])->middleware(['auth'])->name('questions.show');
+Route::post('/quizzes/{id}/questions/{qid}', [App\Http\Controllers\QuestionController::class, 'update'])->middleware(['auth'])->name('questions.update');
+Route::get('/quizzes/{id}/questions/{qid}/delete', [App\Http\Controllers\QuestionController::class, 'delete'])->middleware(['auth'])->name('questions.delete');
+Route::post('/quizzes/{id}/questions/{qid}/delete', [App\Http\Controllers\QuestionController::class, 'destroy'])->middleware(['auth'])->name('questions.destroy');
+
 require __DIR__.'/auth.php';
