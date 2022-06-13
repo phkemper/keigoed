126bdff1d3c459f32c5bb135a3d63895a9d73fe1
Paul Kemper <phkemper@gmail.com>
2022-06-13 06:24:56 +0200

    d

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Laravel Framework Service Providers...
          */
         Illuminate\Auth\AuthServiceProvider::class,
+        BeyondCode\LaravelWebSockets\WebSocketsServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
